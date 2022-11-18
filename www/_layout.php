c736e0434dc1255eef028c42592d4c4fce38ce1b
Volodimir <volodimirnazarisin@gmail.com>
2022-11-18 11:03:07 +0200

    GET POST FILE

--- www/_layout.php
+++ www/_layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css" />
     <title>PV011</title>
     <style>
     html,
@@ -20,28 +21,31 @@
     }
     footer {
         border-top: 1px solid gray;
-        margin: 10px 0 10px;
         padding: 10px 0 10px;
     }
     </style>
 </head>
 <body>
+    <nav>
+        <img src="/img/php.png" alt="logo" class="logo" />
+        <a href="/basics">Введение в РНР</a>
+        <a href="/fundamentals">Основы РНР</a>
+        <a href="/layout">Шаблонизация</a>
+        <a href="/formdata">Данные форм</a>
+    </nav>
+
     <h1>PHP</h1>
-    <a href="/basics">Введение в  РНР</a><br />
-    <a href="/fundamentals">Основы РНР</a><br />
-    <a href="/layout">Шаблонизация</a><br />
 
     <!-- Render body -->
     <?php
+    if( $path_parts[1] === '' ) $path_parts[1] = 'index' ;
     switch( $path_parts[1] ) { // [1] - первая непустая часть (суть контроллер)
-        case '' :
-        case 'index'        : include "index.php";        break;
-        case 'basics'       : include "basics.php";       break;
-        case 'fundamentals' : include "fundamentals.php"; break;
-        case 'layout'       : include "layout.php";       break;
+        case 'index'        : 
+        case 'basics'       : 
+        case 'fundamentals' : 
+        case 'layout'       : 
+        case 'formdata'     : include "{$path_parts[1]}.php" ; break ;
         default             : include "404.php";
-            // ✅ Реализовать шаблонизатор - переход на все страницы сайта по единому шаблону.
-            // ✅ Разработать страницу 404 как часть шаблонных страниц
     }
     ?>
 
